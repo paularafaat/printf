200492372b3fe8154dda31b3d53def32e750c9f3
paula <paula.com>
2023-10-17 20:10:48 +0300

    md

--- _printf.c
+++ _printf.c
@@ -7,79 +7,79 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    int printed = 0;
-    int buff_ind = 0;
-    char buffer[BUFF_SIZE];
+va_list args;
+	int count = 0;
+	int printed = 0;
+	int buff_ind = 0;
+	char buffer[BUFF_SIZE];
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
 
-            if (*format == '%')
-            {
-                buffer[buff_ind++] = '%';
-                if (buff_ind == BUFF_SIZE)
-                {
-                    print_buffer(buffer, &buff_ind);
-                    count += buff_ind;
-                }
-            }
-            else if (*format == 'c')
-            {
-                int ch = va_arg(args, int);
-                buffer[buff_ind++] = ch;
-                if (buff_ind == BUFF_SIZE)
-                {
-                    print_buffer(buffer, &buff_ind);
-                    count += buff_ind;
-                }
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                while (*str)
-                {
-                    buffer[buff_ind++] = *str;
-                    str++;
-                    if (buff_ind == BUFF_SIZE)
-                    {
-                        print_buffer(buffer, &buff_ind);
-                        count += buff_ind;
-                    }
-                }
-            }
-            else if (*format == 'd' || *format == 'i')
-            {
-                int value = va_arg(args, int);
-                printed = print_integer(value);
-                count += printed;
-            }
-        }
-        else
-        {
-            buffer[buff_ind++] = *format;
-            if (buff_ind == BUFF_SIZE)
-            {
-                print_buffer(buffer, &buff_ind);
-                count += buff_ind;
-            }
-        }
+			if (*format == '%')
+			{
+				buffer[buff_ind++] = '%';
+				if (buff_ind == BUFF_SIZE)
+				{
+					print_buffer(buffer, &buff_ind);
+					count += buff_ind;
+				}
+			}
+			else if (*format == 'c')
+			{
+				int ch = va_arg(args, int);
+				buffer[buff_ind++] = ch;
+				if (buff_ind == BUFF_SIZE)
+				{
+					print_buffer(buffer, &buff_ind);
+					count += buff_ind;
+				}
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				while (*str)
+				{
+					buffer[buff_ind++] = *str;
+					str++;
+					if (buff_ind == BUFF_SIZE)
+					{
+						print_buffer(buffer, &buff_ind);
+						count += buff_ind;
+					}
+				}
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int value = va_arg(args, int);
+				printed = print_integer(value);
+				count += printed;
+			}
+		}
+		else
+		{
+			buffer[buff_ind++] = *format;
+			if (buff_ind == BUFF_SIZE)
+			{
+				print_buffer(buffer, &buff_ind);
+				count += buff_ind;
+			}
+		}
 
-        format++;
-    }
+		format++;
+	}
 
-    print_buffer(buffer, &buff_ind);
-    count += buff_ind;
+	print_buffer(buffer, &buff_ind);
+	count += buff_ind;
 
-    va_end(args);
+	va_end(args);
 
-    return count;
+	return count;
 }
 /**
  * print_integer - to print integer
@@ -88,13 +88,13 @@ int _printf(const char *format, ...)
 */
 int print_integer(int value)
 {
-    char buffer[32];
-    int printed = snprintf(buffer, sizeof(buffer), "%d", value);
-    for (int i = 0; i < printed; i++)
-    {
-        putchar(buffer[i]);
-    }
-    return printed;
+	char buffer[32];
+	int printed = snprintf(buffer, sizeof(buffer), "%d", value);
+	for (int i = 0; i < printed; i++)
+	{
+		putchar(buffer[i]);
+	}
+	return printed;
 }
 /**
  * print_buffer - function ot print buffer
@@ -104,13 +104,13 @@ int print_integer(int value)
 */
 void print_buffer(char buffer[], int *buff_ind)
 {
-    if (*buff_ind > 0)
-    {
-        for (int i = 0; i < *buff_ind; i++)
-        {
-            putchar(buffer[i]);
-        }
-    }
+	if (*buff_ind > 0)
+	{
+		for (int i = 0; i < *buff_ind; i++)
+		{
+			putchar(buffer[i]);
+		}
+	}
 
-    *buff_ind = 0;
+	*buff_ind = 0;
 }
