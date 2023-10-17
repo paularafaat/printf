527ee7fa714a8021ad2ba350c9718f826a4bc8aa
paula <paula.com>
2023-10-17 18:15:50 +0300

    testing

--- _printf.c
+++ _printf.c
@@ -6,37 +6,43 @@
  */
 int _printf(const char *format, ...)
 {
-	int chara_printed = 0;
+	int counter = 0;
 	va_list args;
 
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
 	while (*format)
-	{
-		if (*format != '%')
-		{write(1, format, 1);
-		chara_printed++; }
-		else
-		{format++;
-		if (*format == '\0')
-			break;
-		if (*format == '%')
-		{write(1, format, 1);
-		chara_printed++; }
-		else if (*format == 'c')
-		{char c = va_arg(args, int);
-		write(1, &c, 1);
-		chara_printed++; }
-		else if (*format == 's')
-		{char *s = va_arg(args, char*);
-		int _strlen = 0;
+{
+  if (*format == '%')
+    format++;
+      if (*format == '%'){
+          Putchar('%')
+            counter++;
 
-		while (s[_strlen] != '\0')
-			_strlen++;
-		write(1, s, _strlen);
-		chara_printed += _strlen;
-		}
-		} format++; }
-	va_end(args);
-	return (chara_printed); }
+      } else if (*format == 'c') //if after % is c
+      {
+          int ch = va_arg(args, int) //Open variable to store the args from the list
+          putchar(ch) //print it
+          counter++; //increase our counter
+      }
+      else if (*format == 's') //if after % is s so it's string 
+        {
+          char *str = va_arg(args, char*);//start the variable which holding the string
+          while (*str) //looping over the string to print all charaters
+          {
+            putchar(*str); //printing the characters
+            str++; //moving a step forward to print the next letter
+            counter++; //keep tracking the num of letters printed
+          }
+  else {//if there is no % and it's a normal string
+        putchar(*format); //print it as it
+         counter++; //keep tracking the num of letters printed
+        }
+
+    foramt++;
+}
+
+va_end(args);
+}
+return counter;
