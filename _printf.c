43549e8788c05957daaef3f443ded854db783efa
paula <paula.com>
2023-10-17 16:55:10 +0300

    printf proj

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _printf - function that prints %c %s %
+ * @format: pointer
+ * Return: number of chara printed
+ */
+int _printf(const char *format, ...)
+{
+	int chara_printed = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{write(1, format, 1);
+		chara_printed++; }
+		else
+		{format++;
+		if (*format == '\0')
+			break;
+		if (*format == '%')
+		{write(1, format, 1);
+		chara_printed++; }
+		else if (*format == 'c')
+		{char c = va_arg(args, int);
+		write(1, &c, 1);
+		chara_printed++; }
+		else if (*format == 's')
+		{char *s = va_arg(args, char*);
+		int _strlen = 0;
+
+		while (s[_strlen] != '\0')
+			_strlen++;
+		write(1, s, _strlen);
+		chara_printed += _strlen;
+		}
+		} format++; }
+	va_end(args);
+	return (chara_printed); }
